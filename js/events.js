4980fab48216b0d47cc65ed3ed9d8a75050acc26
Kavindu Amarasinghe <amarasinghekavindu@yahoo.com>
2017-11-29 22:11:16 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,19 +1,32 @@
 function getIt(){
   $('p').on('click', function(){
     alert('Hey!');
-});
+  });
 }
 
 function frameIt(){
   $('img').on('load', function(){
     $('img').addClass('tasty');
-});
+  });
 }
 
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now');
+  });
+}
+
+function pressIt(){
+  $(document).on('keydown', function(){
+    
+  });
+}
 
 $(document).ready(function(){
 
 getIt();
 frameIt();
+submitIt();
+pressIt();
 
 });
